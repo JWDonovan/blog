678751aab59de42c59e5598d4538f3363b07a000
JWDonovan <jameswilliamdonovan@gmail.com>
2020-11-16 19:08:06 -0500

    Added demo post to seeds.rb, still needs ActionText and ActiveStorage integration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post = Post.create({
+    title: "How to avoid a Pandas Pandemonium",
+    slug: "how-to-avoid-a-pandas-pandemonium",
+    subtitle: "A deep dive into common Pandas mistakes and what you can do to avoid them. Part I: Writing good code and spotting silent failures.",
+    datetime: "2020-11-15 21:06:00",
+    # body
+    published: true,
+    comments_enabled: true
+})
+
+
+post.hero_image.attach(io: File.open('app/assets/images/panda_pandemonium.jpeg'), filename: 'panda_pandemonium.jpeg', content_type: 'image/jpge')
